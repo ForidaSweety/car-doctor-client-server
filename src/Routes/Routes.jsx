8625c2eb81657c28db68f0773e585434244e2bf9
ForidaSweety <foridayeasmin2000@gmail.com>
2024-02-25 22:07:12 +0600

    commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import BookService from "../pages/BookService/BookService";
+import PrivateRoute from "./PrivateRoute";
+import Bookings from "../pages/Bookings/Bookings";
+import About from "../pages/Home/About/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      },
+      {
+        path: 'book/:id',
+        element: <PrivateRoute> <BookService></BookService></PrivateRoute>,
+        loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+      },
+      {
+        path: 'bookings',
+        element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+      }
+    ]
+  },
+]);
+
+export default router;
